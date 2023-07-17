f42187bdf89d0753d6be68d5c90995dcba743449
并没有什么luck <10255541+yusmart@user.noreply.gitee.com>
2023-07-17 23:05:24 +0800

    button&dialog

---
+++ src/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <div id="button-container">
+      <div class="row">
+        <yzhButton>按钮</yzhButton>
+        <yzhButton @click="fn1" type="primary">按钮</yzhButton>
+        <yzhButton type="success">按钮</yzhButton>
+        <yzhButton type="info">按钮</yzhButton>
+        <yzhButton type="warning">按钮</yzhButton>
+        <yzhButton type="danger">按钮</yzhButton>
+      </div>
+      <div class="row">
+        <yzhButton plain>按钮</yzhButton>
+        <yzhButton plain type="primary">按钮</yzhButton>
+        <yzhButton plain type="success">按钮</yzhButton>
+        <yzhButton plain type="info">按钮</yzhButton>
+        <yzhButton plain type="warning">按钮</yzhButton>
+        <yzhButton plain type="danger">按钮</yzhButton>
+      </div>
+      <div class="row">
+        <yzhButton round>按钮</yzhButton>
+        <yzhButton round type="primary">按钮</yzhButton>
+        <yzhButton round type="success">按钮</yzhButton>
+        <yzhButton round type="info">按钮</yzhButton>
+        <yzhButton round type="warning">按钮</yzhButton>
+        <yzhButton round type="danger">按钮</yzhButton>
+      </div>
+      <div class="row">
+        <yzhButton plain icon="one-icon-check" circle></yzhButton>
+        <yzhButton plain icon="one-icon-camera" circle type="primary"></yzhButton>
+        <yzhButton plain icon="one-icon-bluetoothon" circle type="success"></yzhButton>
+        <yzhButton plain icon="one-icon-addto" circle type="info"></yzhButton>
+        <yzhButton plain icon="one-icon-DND_mode" circle type="warning"></yzhButton>
+        <yzhButton plain icon="one-icon-delete" circle type="danger"></yzhButton>
+      </div>
+      <div class="row">
+        <yzhButton disabled>按钮</yzhButton>
+        <yzhButton disabled type="primary">按钮</yzhButton>
+        <yzhButton disabled type="success">按钮</yzhButton>
+        <yzhButton disabled type="info">按钮</yzhButton>
+        <yzhButton disabled type="warning">按钮</yzhButton>
+        <yzhButton disabled type="danger">按钮</yzhButton>
+      </div>
+    </div>
+    <div id="dialog-container">
+      <!-- <yzh-dialog title="温馨提示"></yzh-dialog> -->
+      <!-- 1. 使用插槽的tip框;插槽由三部分组成，没有传值的话就是默认的插槽，特别注意，记得用  v-slot:插槽名字  指定插槽的名字
+           2. 指定宽度和高度-->
+      <yzhButton type="primary" @click="visiable = true">按钮</yzhButton>
+      <yzh-dialog width="80%" top="300px" :visiable="visiable" @close="close">
+        <template v-slot:title>
+          <h3> 温馨提醒</h3>
+        </template>
+        <template v-slot:body>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </template>
+        <template v-slot:footer>
+          <yzhButton type="primary" @click="visiable = false">确定</yzhButton>
+          <yzhButton @click="visiable = false">取消</yzhButton>
+        </template>
+      </yzh-dialog>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      visiable: false
+    }
+  },
+  methods: {
+    fn1() {
+      console.log(123);
+    },
+    close(e) {
+      // e是接受传过来的事件
+      this.visiable = e
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.row {
+  margin-bottom: 20px;
+
+  .yzhButton {
+    margin-right: 20px;
+  }
+}
+</style>
---
+++ src/components/dialog.vue
@@ -0,0 +1,147 @@
+<template>
+    <div class="yzh-dialog" v-show="visiable">
+        <div id="dialog-container">
+            <!-- self,表示只点击自己才触发，这样不会冒泡 -->
+            <div class="yzh-dialog_wrapper" @click.self="handleClose">
+                <div class=" yzh-dialog" :style="{ width, top }">
+                    <div class="yzh-dialog_header">
+                        <slot name="title">
+                            <span class="yzh-dialog_title">{{ title }}</span>
+                        </slot>
+                        <button class="yzh-dialog_headerbtn" @click="handleClose">
+                            <i class="one-icon-close"></i>
+                        </button>
+                    </div>
+                    <div class="yzh-dialog_body">
+                        <slot name="body">
+
+                        </slot>
+                    </div>
+                    <div class="yzh-dialog_footer">
+                        <slot name="footer">
+                            <yzhButton class="yzhButton">取消</yzhButton>
+                            <yzhButton type="primary">确定</yzhButton>
+                        </slot>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'yzhDialog',
+    props: {
+        title: {
+            type: String,
+            default: '提示'
+        },
+        width: {
+            type: String,
+            default: '50%'
+        },
+        top: {
+            type: String,
+            default: '300px'
+        },
+        visiable: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        handleClose() {
+            // 点击子组件不能直接修改父组件的数据，我们应该触发父组件身上的事件，把新的值传递给父组件（$emit），然后在父组件身上修改
+            // this.visiable = false
+            this.$emit('close', false)
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.yzh-dialog_wrapper {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    overflow: auto;
+    margin: 0;
+    z-index: 2001;
+    background-color: rgba(0, 0, 0, 0.5);
+
+    .yzh-dialog {
+        position: relative;
+        margin: 15vh auto 50px;
+        background: #fff;
+        border-radius: 2px;
+        box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3);
+        box-sizing: border-box;
+        width: 30%;
+
+        &_header {
+            padding: 20px 20px 10px;
+
+            .yzh-dialog_title {
+                line-height: 24px;
+                font-size: 18px;
+                color: #303133;
+            }
+
+            .yzh-dialog_headerbtn {
+                position: absolute;
+                top: 20px;
+                right: 20px;
+                padding: 0;
+                background: transparent;
+                border: none;
+                outline: none;
+                cursor: pointer;
+                font-size: 16px;
+
+                .one-icon-close {
+                    color: 909399
+                }
+            }
+        }
+
+        &_body {
+            padding: 30px 20px;
+            color: #606266;
+            font-size: 14px;
+            word-break: break-all;
+        }
+
+        &_footer {
+            padding: 10px 20px 20px;
+            text-align: right;
+            box-sizing: border-box;
+
+            ::v-deep .yzhButton:first-child {
+                margin-right: 20px;
+            }
+        }
+    }
+}
+
+.dialog-fade-enter-active {
+    animation: fade .3s;
+}
+
+.dialog-fade-leave-active {
+    animation: fade .3s reverse;
+}
+
+@keyframes fade {
+    0% {
+        opacity: 0;
+        transform: translateY(-20px);
+    }
+
+    100% {
+        opacity: 1;
+        transform: translateY(0);
+    }
+}
+</style>
