b5bed7ec61ce6695ba6d891e5a8bd5c88ee096df
并没有什么luck <10255541+yusmart@user.noreply.gitee.com>
2023-07-17 22:16:18 +0800

    button&dialog

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import yzhButton from './components/button.vue'
+import yzhDialog from './components/dialog.vue'
+import './assets/fonts/iconfont.css'
 Vue.config.productionTip = false
-
+Vue.component(yzhButton.name, yzhButton)
+Vue.component(yzhDialog.name, yzhDialog)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
